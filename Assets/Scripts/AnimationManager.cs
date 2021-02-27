d04db7398a42c40eb9fd271f1448889f7947c451
Dinis Costa <scorpaust@hotmail.com>
2021-02-27 14:36:29 +0000

    Animation Manager to handle all game animations.

---
+++ Assets/Scripts/AnimationManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationManager : Singleton<AnimationManager>
+{
+    private Animator anim;
+
+    public void Play(string animationName)
+	{
+		anim.Play(animationName);
+	}
+
+}
