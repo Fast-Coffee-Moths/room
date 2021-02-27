06cb2d8bfd0db95f9968b6b2af023d4bd7713ea5
Dinis Costa <scorpaust@hotmail.com>
2021-02-27 20:38:14 +0000

    Floating thing threat. If not friendly explodes randomly on player approach. If friendly it just floats.

---
+++ Assets/Scripts/FloatingThing.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatingThing : MonoBehaviour, IThreat
+{
+    public float duration { get; set; } 
+    public bool friendly { get; set; }
+    public int level { get; set; }
+    public ThreatState state { get; set; }
+    private bool activated;
+
+    public GameObject player;
+
+    // User Inputs
+    public float degreesPerSecond = 15.0f;
+    public float amplitude = 0.5f;
+    public float frequency = 1f;
+    private Vector3 offset;
+
+    // Position Storage Variables
+    Vector3 posOffset = new Vector3();
+    Vector3 tempPos = new Vector3();
+
+    private float attackRange = 5.0f;
+
+    public void Init()
+	{
+        state = ThreatState.ACTIVE;
+        gameObject.SetActive(true);
+        offset = this.gameObject.transform.position - player.transform.position;
+        activated = true;
+	}
+
+    private void Update()
+    {
+
+        if (activated && friendly)
+		{
+            Float();
+
+        } else if (activated && !friendly)
+		{
+            if (Vector3.Distance(transform.position - offset, player.transform.position) < attackRange)
+			{
+                Float();
+                if (Random.Range(0, 100 / level) > 50)
+                {
+                    Explode();
+                }
+			}
+
+		} else
+		{
+            return;
+		}
+        
+    }
+    
+    public void Deactivate()
+	{
+        activated = false;
+        state = ThreatState.INACTIVE;
+    }
+
+    private void Float()
+	{
+        // Spin object around Y-Axis
+        transform.Rotate(new Vector3(0f, Time.deltaTime * degreesPerSecond, 0f), Space.World);
+
+        // Float up/down with a Sin()
+        tempPos = posOffset;
+        tempPos.y += Mathf.Sin(Time.fixedTime * Mathf.PI * frequency) * amplitude;
+
+        transform.position = tempPos;
+    }
+
+    private void Explode()
+	{
+        //Play particle effects
+        //Player dies and respawns
+        gameObject.SetActive(false);
+        Deactivate();
+	}
+}
