145de4a59ae29b402cc4dfe7b27145e9532659be
Dinis Costa <scorpaust@hotmail.com>
2021-02-27 15:40:23 +0000

    First threat implementation example (weird flashing lights). Threat Manager to handle with threats.

---
+++ Assets/Scripts/FlashingLights.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class FlashingLights : MonoBehaviour, IThreat
+{
+
+    public float duration { get; set; } // The total of seconds the flash wil last
+    public float maxIntensity; // The maximum intensity the flash will reach
+    public Light myLight;        // light
+    public bool friendly { get; set; }
+    public int level { get; set; }
+    public ThreatState state { get; set; }
+
+    public void Init()
+	{
+        StartCoroutine(FlashNow());
+    }
+
+    public IEnumerator FlashNow()
+    {
+        float waitTime = friendly ? duration / 2 : duration / (level * 2);
+
+        while (myLight.intensity < maxIntensity)
+        {
+            myLight.intensity += Time.deltaTime / waitTime;        // Increase intensity
+            yield return null;
+        }
+        while (myLight.intensity > 0)
+        {
+            myLight.intensity -= Time.deltaTime / waitTime;        //Decrease intensity
+            yield return null;
+        }
+        yield return null;
+    }
+
+    public void Deactivate()
+	{
+        StopCoroutine(FlashNow());
+	}
+}
