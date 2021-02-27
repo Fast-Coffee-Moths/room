c65fdf085ab97ecb284e315c326a2b2eb7872cc6
Alberto Lameira <lameira.alberto94@gmail.com>
2021-02-27 20:50:49 +0000

    Interactable Objects: fixed dragging speed

--- Assets/Scripts/Interacter.cs
+++ Assets/Scripts/Interacter.cs
@@ -5,6 +5,17 @@ using UnityEngine;
 public class Interacter : MonoBehaviour
 {
     private InteractableObjectInterface _currentInteractableObject;
+    private float _originalSpeedValue;
+    private bool _isDraggable = false;
+    private bool _isDragging = false;
+    private FirstPersonMovement _movement;
+    [SerializeField] float _speedWhileDraggingObject = 1f;
+
+    private void Awake()
+    {
+        _movement = gameObject.GetComponent<FirstPersonMovement>();
+        _originalSpeedValue = _movement.playerData.speed;
+    }
 
     private void Update()
     {
@@ -12,8 +23,25 @@ public class Interacter : MonoBehaviour
         {
             if (_currentInteractableObject != null)
             {
+                if (_isDraggable)
+                {
+                    ToggleIsDragging();
+                }
                 _currentInteractableObject.Interact();
             }
+        }
+    }
+
+    private void ToggleIsDragging()
+    {
+        if (_isDragging)
+        {
+            _movement.playerData.speed = _originalSpeedValue;
+            _isDragging = false;
+        } else
+        {
+            _movement.playerData.speed = _speedWhileDraggingObject;
+            _isDragging = true;
         }
     }
 
@@ -26,11 +54,18 @@ public class Interacter : MonoBehaviour
             {
                 _currentInteractableObject = obj;
             }
+            DraggableObject drag = other.gameObject.GetComponent<DraggableObject>();
+            if (drag != null)
+            {
+                _isDraggable = true;
+            }
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
         _currentInteractableObject = null;
+        _isDraggable = false;
+        _movement.playerData.speed = _originalSpeedValue;
     }
 }
