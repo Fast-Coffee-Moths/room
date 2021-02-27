7481172b8f3a6d3ce1bab769eca3677e8bd32925
Alberto Lameira <lameira.alberto94@gmail.com>
2021-02-27 12:18:38 +0000

    Interactable Object: Notify Player Ready to Interact

---
+++ Assets/Scripts/Interacter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interacter : MonoBehaviour
+{
+    private GameObject _currentInteractableObject;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        InteractableObject obj = other.gameObject.GetComponent<InteractableObject>();
+        if (obj != null)
+        {
+            _currentInteractableObject = other.gameObject;
+            Debug.Log("Ready to interact with: " + other.gameObject.name);
+        }
+    }
+}
