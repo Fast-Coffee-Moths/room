7a8483daa883f3565f1e957a30e2f0136a03aae2
Alberto Lameira <lameira.alberto94@gmail.com>
2021-02-27 20:02:54 +0000

    Interactable Object: fixed draggable object behaviour

--- Assets/Scripts/Interacter.cs
+++ Assets/Scripts/Interacter.cs
@@ -10,20 +10,22 @@ public class Interacter : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.E))
         {
-            FollowPlayer container = GameObject.Find("DraggableObjectContainer").GetComponent<FollowPlayer>();
-            GameObject bodyInteracter = GameObject.Find("BodyInteracter");
-            container.offset = bodyInteracter.transform.position - transform.position;
-            container.rotation = bodyInteracter.transform.rotation;
-            _currentInteractableObject.Interact();
+            if (_currentInteractableObject != null)
+            {
+                _currentInteractableObject.Interact();
+            }
         }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        InteractableObjectInterface obj = other.gameObject.GetComponent<InteractableObjectInterface>();
-        if (obj != null)
+        if (_currentInteractableObject == null)
         {
-            _currentInteractableObject = obj;
+            InteractableObjectInterface obj = other.gameObject.GetComponent<InteractableObjectInterface>();
+            if (obj != null)
+            {
+                _currentInteractableObject = obj;
+            }
         }
     }
 
